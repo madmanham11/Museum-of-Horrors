bc04ae9be417928370d0b09592e12eaada1f4572
Darion-Quarles <dquarles@student.neumont.edu>
2023-03-02 16:25:12 -0700

    hands halucigen working

---
+++ Assets/Halls/HandLusion.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLusion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    System.Random ran = new System.Random();
+
+    // Start is called before the first frame update
+    [SerializeField] GameObject handslu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+     GotHands();
+        
+    }
+    private void GotHands()
+    {
+        int chance = ran.Next(0, 10000);
+        if (chance >= 9950)
+        {
+            Debug.Log("Start");
+
+            if (!handslu.activeSelf)
+            {
+                handslu.SetActive(true);
+                Invoke("losthands", 1.5f);
+            }
+            
+        }
+    
+
+    }
+
+    private void losthands()
+    {
+        Debug.Log("End");
+
+        handslu.SetActive(false);
+    }
+}
